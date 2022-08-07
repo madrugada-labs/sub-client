9e6bfb32d351589453e6cefc67fca801201f1c8b
fl-y <thedigitalimg@gmail.com>
2022-08-08 04:37:21 +0900

    fix: plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
-import { RouterLoaded } from "@/components/common/RouterLoaded";
 import { Influencer } from "@/constants/app-constants";
 import { GraphQLProvider } from "@/context/GraphQLProvider";
 import { initPackages } from "@/utils/init";
@@ -20,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Script
         defer
         data-domain={Influencer.DOMAIN}
-        src="https://plausible.io/js/plausible.js%22%3E"
+        src="https://plausible.io/js/plausible.js"
         strategy="lazyOnload"
       />
       <div className="min-h-[100vh] min-w-[100vw] bg-bg">
